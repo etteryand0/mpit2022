9f3d061143d6a1797e27b452f383f0890e1f31d5
etteryand0 <etteryand@gmail.com>
2022-04-21 21:17:02 +0900

    :zap: Script to run graphql server

--- apps/backend/src/main.js
+++ apps/backend/src/main.js
@@ -1,1 +1,51 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT_EXPRESS || 8001;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log('Express server is running at port ' + PORT)
+});
+
+const { ApolloServer } = require('apollo-server');
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const { typeDefs } = require('./graphql/typeDefs');
+const { resolvers } = require('./graphql/resolvers');
+
+const jwt = require('jsonwebtoken');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const authorization = req.headers;
+
+    const salt = 10;
+    return {
+      prisma,
+      salt,
+      tokenError: new Error("Invalid token"),
+      permissionError: new Error("Permission denied"),
+      checkToken: async () => {
+        const SECRET = process.env.AUTH_TOKEN;
+
+        const decoded = jwt.decode(authorization, SECRET, (err, decoded) => {
+          if (err)
+            return null;
+          return decoded;
+        });
+        return decoded;
+      },
+    }
+  }
+});
+
+require('dotenv').config();
+
+server.listen().then(({ url }) => {
+  console.log(`Server up & running at ${url}`)
+})
